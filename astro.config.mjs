0534750244d00c3c872a1815c020fcc1eb97e826
jesus-traslavina_xpo <jesus.traslavina@xpo.com>
2026-01-20 11:57:42 -0500

    AstroBoy

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bartimeo.app',
+  site: 'https://pjr-app-data.online',
   adapter: vercel(),
   integrations: [
     sitemap({
